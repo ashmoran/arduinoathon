55ceff5e25d4d912f4400c8af077ff4bab07ee6e
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-10-16 21:52:42 +0100

    Green

--- spec/arduinoathon_spec.rb
+++ spec/arduinoathon_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 require 'arduinoathon'
 
 describe "arduinoathon" do
-  it "compares numbers" do
-    (1 + 1).should eq 34
+  let(:arduinoathon) { Arduinoathon.new }
+
+  it "compares the same number as true" do
+    arduinoathon.compare(1 + 1, 2).should be_true
+  end
+
+  it "compares the different numbers as false" do
+    arduinoathon.compare(1 + 1, 3).should be_false
   end
 end
